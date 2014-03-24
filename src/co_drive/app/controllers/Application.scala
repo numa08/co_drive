fb520a7b1bbc0db49c8d3b7bb83095bfe62cd804
numa08 <n511287@gmail.com>
2014-03-25 02:47:19 +0900

    ログイン機能の実装
    トップ画面よりGitHubのアカウントを利用してログインができる

--- src/co_drive/app/controllers/Application.scala
+++ src/co_drive/app/controllers/Application.scala
@@ -1,10 +1,21 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc._
+import org.pac4j.play.scala._
 
-object Application extends Controller{
+object Application extends ScalaController{
 
-  def index = Action {
-    Ok(views.html.index())
+  def index = Action { request =>
+    val newSession = getOrCreateSessionId(request)
+    val navbarButton = Option(getUserProfile(request)).map{p =>
+      val avatarUrl = p.getAttribute("avatar_url").toString
+      val screenName = p.getAttribute("login").toString
+      views.html.githubuserbutton(screenName, avatarUrl)
+    }.getOrElse{
+      val urlGithub = getRedirectionUrl(request, newSession, "GitHubClient", "/?0")
+      views.html.githubloginbutton(urlGithub)
+    }
+    Ok(views.html.index(navbarButton))
   }
+
 }
--- src/co_drive/build.sbt
+++ src/co_drive/build.sbt
@@ -8,11 +8,17 @@ libraryDependencies ++= Seq(
   cache
 )
 
+resolvers ++= Seq(
+  "Sonatype snapshots repository" at "https://oss.sonatype.org/content/repositories/snapshots/"
+)
+
 libraryDependencies ++= Seq(
   "org.webjars" %% "webjars-play" % "2.2.1-2",
   "org.webjars" % "bootstrap" % "3.1.1",
-  "org.webjars" % "jquery" % "2.1.0-2"
+  "org.webjars" % "jquery" % "2.1.0-2",
+  "org.webjars" % "font-awesome" % "4.0.3",
+  "org.pac4j" % "play-pac4j_scala" % "1.2.0-SNAPSHOT",
+  "org.pac4j" % "pac4j-oauth" % "1.5.0-SNAPSHOT"
 )
-
 
 play.Project.playJavaSettings
